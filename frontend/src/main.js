6b1e9560990bf2f16b01b871eecc53aaa086cc82
MARKUPsafe <emmanuelyakam1@gmail.com>
2025-03-06 13:59:35 +0100

    reconfiguration de bootstrapVue. utilisation de bootstrapVue-next

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,16 +5,14 @@ import App from './App.vue'; //App.vue est le composant racine de l'application 
 import router from './router'; //le fichier router/index.js contient la configuration des routes de l'application, c'est-à-dire l'association des chemins URL aux composants Vue correspondants. Cela permet à l'application de naviguer entre différentes vues sans recharger la page. Ici router fait référence à l'instance de Vue Router.
 import LoginPage from './views/LoginPage.vue';
 import HomePage from './views/HomePage.vue';
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import Vue from 'vue'
+import BootstrapVueNext from 'bootstrap-vue-next';
+
 
 /*import des composants de bootstrap */
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // Si on veut utiliser les composants JavaScript (comme les modals, dropdowns, etc.)
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
 /*
     createApp(App) crée une nouvelle instance de l'application Vue avec le composant principal App.vue. 
     Le composant App.vue devient ainsi le point d'entrée dans l'arbre des composants de l'application Vue.
@@ -34,6 +32,8 @@ app.component('home', HomePage)
     En utilisant use, Vue intègre le routeur, ce qui permet d'afficher les composants en fonction de l'url.
 */
 app.use(router);
+//utilisation de bootstrapVue
+app.use(BootstrapVueNext);
 
 /*
     app.mount('#app') attache l'instance Vue créée à un élément du DOM. 
