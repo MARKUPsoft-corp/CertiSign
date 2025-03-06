53aa1fa926c038a917d71f06518190316ba66c8c
MARKUPsafe <emmanuelyakam1@gmail.com>
2025-03-06 13:39:38 +0100

    test de commit dans vs code directement

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -2,12 +2,13 @@
 
 <template>
     <h1>BIENVENU {{ nom }}</h1>
+    
 </template>
 
 <script setup>
 
     import { ref } from 'vue';
     const nom = ref("")
-    nom.value = prompt("entrez votre nom")
+    nom.value = prompt("entrez votre nom") 
 
 </script>
