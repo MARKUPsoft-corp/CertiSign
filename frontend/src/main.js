8fa3f11d680e630202d1e6136c1f45a436fb87d9
MARKUPsafe <emmanuelyakam1@gmail.com>
2025-03-06 13:25:31 +0100

    ajout de bootstrap-vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,16 @@ import App from './App.vue'; //App.vue est le composant racine de l'application 
 import router from './router'; //le fichier router/index.js contient la configuration des routes de l'application, c'est-à-dire l'association des chemins URL aux composants Vue correspondants. Cela permet à l'application de naviguer entre différentes vues sans recharger la page. Ici router fait référence à l'instance de Vue Router.
 import LoginPage from './views/LoginPage.vue';
 import HomePage from './views/HomePage.vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import Vue from 'vue'
 
 /*import des composants de bootstrap */
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // Si on veut utiliser les composants JavaScript (comme les modals, dropdowns, etc.)
-
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 /*
     createApp(App) crée une nouvelle instance de l'application Vue avec le composant principal App.vue. 
     Le composant App.vue devient ainsi le point d'entrée dans l'arbre des composants de l'application Vue.
