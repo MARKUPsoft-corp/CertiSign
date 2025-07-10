b76175aa7841116ace48e28e67947646c454be2d
MARKUPsoft-corp <emmanuelyakam1@gmail.com>
2025-07-10 12:41:46 +0100

    dernier test du deploy continu

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Répertoire du projet (à ajuster selon votre serveur)
+# Répertoire du projet 
 PROJECT_DIR="/home/ssatl/Documents/Doc@uthANTIC"
 LOG_FILE="$PROJECT_DIR/logs/deploy.log"
 
