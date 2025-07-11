676fa44f2ec4860aa625bf5c28abd9e61e967375
MARKUPsoft-corp <emmanuelyakam1@gmail.com>
2025-07-11 10:10:18 +0100

    le signataire signe directement quelques améliorations 4

--- frontend/src/views/SignSimpleSigner.vue
+++ frontend/src/views/SignSimpleSigner.vue
@@ -21,8 +21,17 @@ const emit = defineEmits(['close']);
 // S'assurer que le nom d'organisation est présent dans l'objet user stocké
 onMounted(() => {
   const currentUser = AuthService.getCurrentUser();
-  if (currentUser && props.organizationName && !currentUser.organization) {
-    AuthService.updateCurrentUser({ organization: props.organizationName });
+  if (currentUser) {
+    const updates = {};
+    if (props.organizationName && !currentUser.organization) {
+      updates.organization = props.organizationName;
+    }
+    if (currentUser.organization && currentUser.organization.id) {
+      updates.organizationId = currentUser.organization.id;
+    }
+    if (Object.keys(updates).length) {
+      AuthService.updateCurrentUser(updates);
+    }
   }
 });
 </script> 
